5332d3d1637e44acfa08430f4bc7ed74bdebfa3e
MAYANK <mayank372002@gmail.com>
2023-08-18 01:13:36 +0530

    fixed bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Route, Routes } from "react-router-dom";
 import IndexPage from './pages/IndexPage';
-import LoginPage from './pages/loginPage';
+import LoginPage from './pages/LoginPage';
 import Layout from './Layout';
 import RegisterPage from './pages/RegisterPage';
 import axios from 'axios';
