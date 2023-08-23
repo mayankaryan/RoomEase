d617f38af839ad45ab3d82a158899a2aedd6b79d
MAYANK <mayank372002@gmail.com>
2023-08-23 19:41:43 +0530

    link frontend debug cors failure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,6 @@ import PlacesFormPage from './pages/PlacesFormPage';
 import PlacePage from './pages/PlacePage';
 import BookingsPage from './pages/BookingsPage';
 import BookingPage from './pages/BookingPage';
-// axios.defaults.baseURL = 'http://localhost:4000';
 axios.defaults.baseURL = 'https://roomease.onrender.com';
 axios.defaults.withCredentials = true;
 
