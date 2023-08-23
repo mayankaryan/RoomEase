6633062f4110f11b0e8f3e559b92598f07ced005
MAYANK <mayank372002@gmail.com>
2023-08-23 20:13:30 +0530

    fixed another bug

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
     credentials: true,
-    origin: 'https://room-ease-mhz6-m9gwwd29q-mayankaryan.vercel.app',
+    origin: '*',
 }));
 
 mongoose.connect(process.env.MONGO_URL);
