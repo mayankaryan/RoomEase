6e756ebe360fd25b9b88d5c6a42ad0917972db37
MAYANK <mayank372002@gmail.com>
2023-09-28 23:11:47 +0530

    Fixed Bugs

--- api/index.js
+++ api/index.js
@@ -21,11 +21,13 @@ const jwtSecret = 'sfdfseofjoiejfoegrsfjaer';
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
-app.use(cors({
-    origin: "https://room-ease.vercel.app",
-    credentials: true
-}));
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://room-ease.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT,PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 mongoose.connect(process.env.MONGO_URL);
 
 function getUserDataFromReq(req) {
